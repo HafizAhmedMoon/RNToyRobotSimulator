5fc74841ba4ff41f58ae802fd875e905460ec68d
Ahmed Raza <hafizahmedmoon@gmail.com>
2017-10-14 03:09:56 +0500

    refactor: remove unused export

--- app/containers/RobotSimulator.js
+++ app/containers/RobotSimulator.js
@@ -2,7 +2,7 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { connect } from "react-redux";
 
-export class RobotSimulator extends React.Component<{}> {
+class RobotSimulator extends React.Component<{}> {
 
   render() {
     const {robot} = this.props;
