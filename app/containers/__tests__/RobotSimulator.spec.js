957d29cfcbb574a59aebd31f490118938205f48f
Ahmed Raza <hafizahmedmoon@gmail.com>
2017-10-14 18:07:55 +0500

    test: add test cases for RobotSimulator

--- app/containers/__tests__/RobotSimulator.spec.js
+++ app/containers/__tests__/RobotSimulator.spec.js
@@ -2,20 +2,61 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import configureStore from 'redux-mock-store';
 import RobotSimulator from '../RobotSimulator';
+import { Command } from "../../model/Command";
 
 const mockStore = configureStore();
 
 const initialState = {
-  robot: {},
+  robot: {
+    output: ''
+  },
 };
 
 describe("App Component", () => {
+
   it('renders as expected', () => {
-    const tree = shallow(
+    const wrapper = shallow(
       <RobotSimulator/>, {
         context: {store: mockStore(initialState)}
       }
     );
-    expect(tree.dive()).toMatchSnapshot();
+    expect(wrapper.dive()).toMatchSnapshot();
   });
+
+  it('should receive command from CommandInput', () => {
+    const onCommandReceive = jest.spyOn(RobotSimulator.WrappedComponent.prototype, 'onCommandReceive');
+    const wrapper = shallow(
+      <RobotSimulator/>, {
+        context: {store: mockStore(initialState)}
+      }
+    );
+    const wrapper2 = wrapper.dive();
+    const CommandInput = wrapper2.find('CommandInput');
+    expect(CommandInput).toHaveLength(1);
+
+    CommandInput.simulate('commandReceive', Command.parse('MOVE'));
+
+    expect(onCommandReceive.mock.calls).toHaveLength(1);
+    expect(onCommandReceive.mock.calls[0][0]).toEqual(Command.parse('MOVE'));
+
+    onCommandReceive.mockRestore();
+  });
+
+  it('should handle invalid command', () => {
+    const onCommandReceive = jest.spyOn(RobotSimulator.WrappedComponent.prototype, 'onCommandReceive');
+    const wrapper = shallow(
+      <RobotSimulator/>, {
+        context: {store: mockStore(initialState)}
+      }
+    );
+    const wrapper2 = wrapper.dive();
+    const CommandInput = wrapper2.find('CommandInput');
+    CommandInput.simulate('commandReceive', Command.parse('Some Invalid COMMAND'));
+
+    expect(onCommandReceive.mock.calls).toHaveLength(1);
+    expect(onCommandReceive.mock.calls[0][0].isInvalid).toEqual(true);
+
+    onCommandReceive.mockRestore();
+  });
+
 });
